680165eeab970655efb3c546c8e00471395548ec
aofdev <aof.dev@gmail.com>
2020-12-30 16:08:46 +0700

    [.] Change print message

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def insert(mongo_uri: str, mongo_db: str, mongo_collection: str, jsonfile: str) 
             collection = db[mongo_collection]
             collection.insert_one(raw_data_from_file)
             client.close()
-            print("Inset Done!")
+            print("Insert Success ✅")
         except errors.DuplicateKeyError:
-            print("Duplicate key!")
+            print("Duplicate key ⚠️")
         except Exception as err:
-            print(f"Error somethings {err}")
+            print(f"Error something went wrong {err}")
 
 
 if __name__ == "__main__":
